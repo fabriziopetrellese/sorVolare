bb9876d8c40172d2d0b28e5a870623ec2172971b
Fabrizio Petrellese <petrellesefabrizio@icloud.com>
2024-10-20 18:03:52 +0200

    Initial Commit

---
+++ sorVolare/sorVolareApp.swift
@@ -0,0 +1,17 @@
+//
+//  sorVolareApp.swift
+//  sorVolare
+//
+//  Created by Fabrizio Petrellese on 20/10/24.
+//
+
+import SwiftUI
+
+@main
+struct sorVolareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sorVolareTests/sorVolareTests.swift
@@ -0,0 +1,17 @@
+//
+//  sorVolareTests.swift
+//  sorVolareTests
+//
+//  Created by Fabrizio Petrellese on 20/10/24.
+//
+
+import Testing
+@testable import sorVolare
+
+struct sorVolareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
