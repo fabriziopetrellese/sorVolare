947e8f7f11a9bae2996e00577b3f42b426c901e3
Fabrizio Petrellese <petrellesefabrizio@icloud.com>
2024-10-25 11:46:12 +0200

    prima della ContentView

---
+++ sorVolare/DateModal.swift
@@ -0,0 +1,8 @@
+//
+//  DateModal.swift
+//  sorVolare
+//
+//  Created by Fabrizio Petrellese on 25/10/24.
+//
+
+import Foundation
---
+++ sorVolare/Functions.swift
@@ -0,0 +1,8 @@
+//
+//  Functions.swift
+//  sorVolare
+//
+//  Created by Fabrizio Petrellese on 25/10/24.
+//
+
+import Foundation
