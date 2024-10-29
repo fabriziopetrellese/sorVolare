0a85574ed1ea9c22ae4e70ca27aef810320d99fc
Fabrizio Petrellese <petrellesefabrizio@icloud.com>
2024-10-29 15:57:15 +0100

    restart

--- sorVolare/ContentView.swift
+++ sorVolare/ContentView.swift
@@ -6,81 +6,17 @@
 //
 
 import SwiftUI
-import MapKit
 
 struct ContentView: View {
     
-    @State private var selection: Int = 0
-    @State private var consigli = [Consiglio(title: "Consiglio 1", file: "iphone180", description: "descrizione consiglio 1"), Consiglio(title: "Consiglio 2", file: "airport", description: "descrizione consiglio 2"), Consiglio(title: "Consiglio 3", file: "durante", description: "descrizione consiglio 3"), Consiglio(title: "Consiglio 4", file: "turbo", description: "descrizione consiglio 4"), Consiglio(title: "Consiglio 5", file: "landing", description: "descrizione consiglio 5"), Consiglio(title: "Consiglio 6", file: "DECOLLO", description: "descrizione consiglio 6")]
-    @State private var steps = [Step(title: "rullaggio", file: "checkin"), Step(title: "decollo", file: "entrata"), Step(title: "atterraggio", file: "deco"), Step(title: "turbolenze", file: "atter"), Step(title: "step 5", file: "")]
-    
-    
     var body: some View {
         
-        TabView(selection: $selection) {
-            
-            FirstView()
-                .tabItem {
-                    Label("Go to flight", systemImage: "airplane.departure")
-                        .accentColor(.primary)
-                }.tag(0)
-            
-//            AdvicesView(consigli: $consigli)
-                RelaxingView()
-                .tabItem {
-                    Label("Relaxing", systemImage: "apple.meditate").font(.system(size: 10))
-                        .accentColor(.primary)
-                }
-                .tag(1)
-            
-            ProgressStateView(steps: $steps)
-                .tabItem {
-                    Label("Progress", systemImage: "face.smiling")
-                        .accentColor(.primary)
-                }
-        }//TabView
+        Text("Ciao")
         
     } //body
     
 }//ContentView
 
-
-// Pickers
-/*
- VStack(spacing: 20) {
- HStack {
- Text("Fly from:")
- .font(.title3)
- .padding(.leading)
- 
- Picker("Select", selection: $flyFromSelection) {
- ForEach(locations) { location in
- Text(location.capitale)
- .tag(location as Location?)
- }
- }.pickerStyle(.automatic)
- 
- Spacer()
- }
- 
- HStack {
- Text("Fly to:")
- .font(.title3)
- .padding(.leading)
- 
- Picker("Select", selection: $flyToSelection) {
- ForEach(locations) { location in
- Text(location.capitale)
- .tag(location as Location?)
- }
- 
- }.pickerStyle(.automatic)
- 
- Spacer()
- }
- }
- .padding()
- */
 
 
 #Preview {
