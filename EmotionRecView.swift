021d9266879360ace469c87e8445e87efff0e405
CristianCir <cristiancirella@icloud.com>
2024-11-01 14:43:59 +0100

    AGGIUNTO MODELLO CREATO CON CREATE ML "ML_FACE_TRAINED" E AGGIUNTO VIEW "EmotionRecView"

---
+++ EmotionRecView.swift
@@ -0,0 +1,18 @@
+//
+//  EmotionRecView.swift
+//  sorVolare
+//
+//  Created by Cristian Cirella on 01/11/24.
+//
+
+import SwiftUI
+
+struct EmotionRecView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmotionRecView()
+}
